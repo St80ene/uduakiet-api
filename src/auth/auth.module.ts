e634773c923671781350dc9d0e08007701221287
St80ene <etienejames5@gmail.com>
2026-09-25 16:02:00 +0100

    refactor(auth): remove unused JwtModule import from auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../resources/users/entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { UserAuth } from './entities/user_auth.entity';
 import { AuditLog } from '../resources/audit_logs/entities/audit_log.entity';
